500083a0906d5f5e8cc3eb081d1b8daaaec3c0e1
LorenzoBondan <lbbondan@gmail.com>
2024-04-21 21:46:57 -0300

    custom border to active item in menu

--- frontend/src/pages/Home/MainComponents/index.tsx
+++ frontend/src/pages/Home/MainComponents/index.tsx
@@ -1,32 +1,43 @@
 import ModuleCard from "Components/ModuleCard";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import mdpImage from 'assets/images/mdp.jpg';
 import mdfImage from 'assets/images/mdf.jpg';
 import aluminiumImage from 'assets/images/aluminium.jpg';
+import packagingImage from 'assets/images/packaging.jpg';
 
 const MainComponents = () => {
-    return(
+    const location = useLocation();
+    
+    return (
         <div className='home-container'>
             <div className="row">
-                <div className="col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column">
+                <div className='col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column'>
                     <NavLink to="/home/mdp">
-                        <ModuleCard title='MDP' imgUrl={mdpImage}/>
+                        <div className={`${location.pathname === "/home/mdp" ? "active-card" : ""}`}>
+                            <ModuleCard title='MDP' imgUrl={mdpImage}/>
+                        </div>
                     </NavLink>
                 </div>
-                <div className="col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column">
+                <div className='col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column'>
                     <NavLink to="/home/mdf">
-                        <ModuleCard title='MDF' imgUrl={mdfImage}/>
+                        <div className={`${location.pathname === "/home/mdf" ? "active-card" : ""}`}>
+                            <ModuleCard title='MDF' imgUrl={mdfImage}/>
+                        </div>
                     </NavLink>
                 </div>
-                <div className="col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column">
+                <div className='col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column'>
                     <NavLink to="/home/aluminium">
-                        <ModuleCard title='Alumínios' imgUrl={aluminiumImage}/>
+                        <div className={`${location.pathname === "/home/aluminium" ? "active-card" : ""}`}>
+                            <ModuleCard title='Alumínios' imgUrl={aluminiumImage}/>
+                        </div>
                     </NavLink>
                 </div>
-                <div className="col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column">
-                    <NavLink to="/home/packaging">
-                        <ModuleCard title='Embalagem' imgUrl={aluminiumImage}/>
-                    </NavLink>
+                <div className='col-sm-6 col-lg-6 col-xl-6 col-xxl-4 modules-column'>
+                    <div className={`${location.pathname === "/home/packaging" ? "active-card" : ""}`}>
+                        <NavLink to="/home/packaging">
+                            <ModuleCard title='Embalagem' imgUrl={packagingImage}/>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
