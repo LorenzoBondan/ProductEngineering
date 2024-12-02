adbc5bb7ece866cce3108a78ba66723b0e027f47
LorenzoBondan <lbbondan@gmail.com>
2024-12-02 20:49:45 -0300

    deleted old frontend

--- frontend/src/services/Reports/reportsService.ts
+++
@@ -1,9 +0,0 @@
-import { requestBackend } from "util/requests";
-
-export function report(id: number) {
-    return requestBackend({
-        url: `/fatherReport/${id}`, 
-        withCredentials: true,
-        responseType: 'blob' 
-    });
-}
