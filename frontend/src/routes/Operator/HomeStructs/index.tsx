29d4cf7cd2a13966d0906d879c5c8999edde043e
LorenzoBondan <lbbondan@gmail.com>
2025-02-04 17:24:56 -0300

    deleted unnecessary home struct items

--- frontend/src/routes/Operator/HomeStructs/index.tsx
+++ frontend/src/routes/Operator/HomeStructs/index.tsx
@@ -1,8 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import './styles.css';
 import mdpImage from '../../../assets/images/mdp.jpg';
-import mdfImage from '../../../assets/images/mdf.jpg';
-import aluminiumImage from '../../../assets/images/aluminium.jpg';
 import modulationImage from '../../../assets/images/modulation.jpg';
 import ModuleCard from '../../../components/ModuleCard';
 
@@ -13,16 +11,10 @@ const HomeStruct = () => {
             <h1 className='home-title'>Estruturas</h1>
             <div className="components-catalog-grid mt20 mb20 container">
                 <NavLink to="/mdpstruct">
-                    <ModuleCard title='MDP' imgUrl={mdpImage}/>
-                </NavLink>
-                <NavLink to="/mdfstruct">
-                    <ModuleCard title='MDF' imgUrl={mdfImage}/>
+                    <ModuleCard title='MDP/MDF' imgUrl={mdpImage}/>
                 </NavLink>
                 <NavLink to="/modulationstruct">
-                    <ModuleCard title='Modulação' imgUrl={modulationImage}/>
-                </NavLink>
-                <NavLink to="/aluminiumstruct">
-                    <ModuleCard title='Alumínios' imgUrl={aluminiumImage}/>
+                    <ModuleCard title='Modulação/Alumínios' imgUrl={modulationImage}/>
                 </NavLink>
             </div>
         </div>
